18cba6980a374ebe5258452d140d280998fd325d
tomjfrog <tomj@jfrog.com>
2024-11-26 10:47:26 -0600

    Auth Token 2

--- .github/workflows/create-release-bundle.sh
+++ .github/workflows/create-release-bundle.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <ACCESS_TOKEN> <SIGNING_KEY_NAME> <RELEASE_BUNDLE_VERSION> <BUILD_NAME> <BUILD_NUMBER>"
+if [ $# -lt 6 ]; then
+  echo "Usage: $0 <RT_TOKEN> <SIGNING_KEY_NAME> <RELEASE_BUNDLE_VERSION> <BUILD_NAME> <BUILD_NUMBER>"
   exit 1
 fi
 
 # Parameters
-ACCESS_TOKEN=$1
+RT_TOKEN=$1
 SIGNING_KEY_NAME=$2
 RELEASE_BUNDLE_NAME=$3
 RELEASE_BUNDLE_VERSION=$4
 BUILD_NAME=$5
 BUILD_NUMBER=$6
 
-#OPTIONAL_HEADERS=$3
-
 curl --request POST \
   --url 'https://tomjfrog.jfrog.io/lifecycle/api/v2/release_bundle?project=default&async=true' \
-  --header 'Authorization: Bearer $ACCESS_TOKEN' \
+  --header 'Authorization: Bearer $RT_TOKEN' \
   --header 'Content-Type: application/json' \
   --header 'X-JFrog-Signing-Key-Name: $SIGNING_KEY_NAME' \
   --data '{
