f13d6b84940b95cfd46c2acd28a10d92825ada08
tomjfrog <tomj@jfrog.com>
2024-11-26 13:32:46 -0600

    Adding in Promotion to QA

---
+++ .github/workflows/promote-release-bundle.sh
@@ -0,0 +1,40 @@
+if [ $# -lt 7 ]; then
+  echo "Usage: $0 <RT_TOKEN> <SIGNING_KEY_NAME> <BUNDLE_NAME> <BUNDLE_VERSION> <ENVIRONMENT> <INCLUDED_REPOSITORY_KEYS> OVERWRITE_EXISTING_ARTIFACTS"
+  exit 1
+fi
+
+
+RT_TOKEN=$1
+SIGNING_KEY_NAME=$2
+BUNDLE_NAME=
+BUNDLE_VERSION=
+ENVIRONMENT=$1
+INCLUDED_REPOSITORY_KEYS=$2
+OVERWRITE_EXISTING_ARTIFACTS=$3
+
+# Tshooting
+echo SIGNING_KEY_NAME=$2
+echo BUNDLE_NAME=
+echo BUNDLE_VERSION=
+echo ENVIRONMENT=$1
+echo INCLUDED_REPOSITORY_KEYS=$2
+echo OVERWRITE_EXISTING_ARTIFACTS=$3
+
+
+payload=$(cat EOF
+  {
+  	"environment": "QA",
+  	"included_repository_keys": [
+  		$INCLUDED_REPOSITORY_KEYS
+  	],
+  	"overwrite_existing_artifacts": $OVERWRITE_EXISTING_ARTIFACTS
+  }
+EOF
+)
+
+curl --request POST \
+  --user $RT_TOKEN \
+  --url "https://tomjfrog.jfrog.io/lifecycle/api/v2/promotion/records/$BUNDLE_NAME/$BUNDLE_VERSION" \
+  --header 'Content-Type: application/json' \
+  --header "X-JFrog-Signing-Key-Name: $SIGNING_KEY_NAME" \
+  --data "$payload"
